340c662390365d64e115150e42e31542d5d5a69a
aaharidy <abdoharidy72@gmail.com>
2023-09-20 04:23:11 +0300

    fixed notice issue

--- client/index.tsx
+++ client/index.tsx
@@ -1,9 +1,11 @@
 import { render } from '@wordpress/element';
 import { doAction } from '@wordpress/hooks';
+import '@wordpress/core-data';
+import '@wordpress/notices';
 import PageLayout from './layout';
 import './style.scss';
 
-const appRoot = document.getElementById( 'qsmtp-admin-root' );
-render( <PageLayout />, appRoot );
+const appRoot = document.getElementById('qsmtp-admin-root');
+render(<PageLayout />, appRoot);
 
-doAction( 'QuillSMTP.Admin.PluginsLoaded' );
+doAction('QuillSMTP.Admin.PluginsLoaded');
