b31b376f7de1dc347b9c6785c58fe562545bee53
mdmag <md.magdy.sa@gmail.com>
2024-05-13 03:07:57 +0300

    Housekeeping

--- src/mailers/mailers-register/mailgun/hooks/mailer.ts
+++ src/mailers/mailers-register/mailgun/hooks/mailer.ts
@@ -2,6 +2,7 @@
  * WordPress Dependencies
  */
 import { addFilter } from '@wordpress/hooks';
+import { __ } from '@wordpress/i18n';
 
 /**
  * Internal Dependencies
@@ -13,7 +14,42 @@ addFilter(
 	'QuillSMTP/Mailgun/ImplementIntegrationModuleSettings',
 	(settings, slug: string) => {
 		if (slug === 'mailgun') {
-			settings.render = Connect;
+			settings.connectionSettings =
+			{
+				main: {
+					accounts: {
+						auth: {
+							type: 'credentials',
+							fields: {
+								api_key: {
+									label: __('Private API Key', 'quillsmtp'),
+									type: 'password',
+									required: true,
+								},
+								domain_name: {
+									label: __('Domain Name', 'quillsmtp'),
+									type: 'text',
+									required: true,
+								},
+								region: {
+									label: __('Region', 'quillsmtp'),
+									type: 'select',
+									options: [
+										{
+											label: __('US', 'quillsmtp'),
+											value: 'us',
+										},
+										{
+											label: __('EU', 'quillsmtp'),
+											value: 'eu',
+										},
+									],
+								},
+							},
+						},
+					},
+				}
+			};
 		}
 		return settings;
 	}
